1d50fb4e287ff3fc20f13afa966cdcb681a2632c
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-05-17 17:28:28 -0700

    Added Selection Sort algorithm

--- src/main/java/com/gkonovalov/algorithms/arrays/sorting/SelectionSort.java
+++ src/main/java/com/gkonovalov/algorithms/arrays/sorting/SelectionSort.java
@@ -6,6 +6,9 @@ package com.gkonovalov.algorithms.arrays.sorting;
  * Selection Sort is a simple sorting algorithm that works by repeatedly
  * finding the minimum element from the unsorted part of the array and
  * swapping it with the element at the beginning of the unsorted part.
+ * It's important to note that Selection Sort has a quadratic time complexity,
+ * making it less efficient compared to other sorting algorithms like
+ * Merge Sort or Quick Sort.
  * </p>
  * Runtime Complexity: O(n^2)
  * Space Complexity: O(1)

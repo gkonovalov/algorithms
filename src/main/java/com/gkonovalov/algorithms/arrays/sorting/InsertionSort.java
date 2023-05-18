eec99480e9fa706da43164707078ba10255a79d2
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-05-17 18:09:03 -0700

    Removed word 'Essential'

--- src/main/java/com/gkonovalov/algorithms/arrays/sorting/InsertionSort.java
+++ src/main/java/com/gkonovalov/algorithms/arrays/sorting/InsertionSort.java
@@ -3,12 +3,9 @@ package com.gkonovalov.algorithms.arrays.sorting;
 /**
  * Created by Georgiy Konovalov on 17/05/2023.
  * <p>
- * Insertion Sort is a simple sorting algorithm that works by dividing the input array
- * into a sorted and an unsorted portion. It iterates over the unsorted portion,
- * comparing each element with the elements in the sorted portion and inserting it
- * at the correct position. This process is repeated until the entire array
- * is sorted. It's important to note that Insertion Sort has a quadratic
- * time complexity, making it less efficient compared to other
+ * Insertion Sort has a time complexity of O(n^2), but it performs fewer
+ * comparisons and swaps than Selection Sort on average.
+ * This algorithm is less efficient compared to other
  * sorting algorithms like Merge Sort or Quick Sort.
  * </p>
  * Runtime Complexity: O(n^2)

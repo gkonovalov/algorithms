e8779c47e22849e4a25f46416639bde6e46bbc18
George Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-19 17:32:39 -0700

    Base block for the Graph construction.

---
+++ src/main/java/com/gkonovalov/datastructures/graphs/representation/GraphNode.java
@@ -0,0 +1,32 @@
+package com.gkonovalov.datastructures.graphs.representation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Georgiy Konovalov on 18/06/2023.
+ * <p>
+ * Base block for the Graph construction.
+ *
+ * The Graph Node based on Adjacency List representation which is more efficient than the Adjacency Matrix,
+ * especially for sparse graphs. Traversing the graph takes O(|V| + |E|) time complexity. Also, adding or
+ * removing a Vertex or Edge is simpler and less expensive than with an Adjacency Matrix. However, checking
+ * if an Edge exists between two nodes takes O(degree(V)) time complexity which slower than with Adjacency Matrix.
+ * </p>
+ * Runtime Complexity: O(1) - add Edge and add Vertex,
+ *                     O(|V|+|E|) - removing a Vertex,
+ *                     O(|E|) - removing a Edge,
+ *                     O(degree(V)) - Edge between Vertices (checking for adjacency),
+ *                     O(|V| + |E|) - traversing the graph.
+ * Space Complexity:   O(|V|+|E|), worst case O(|V|^2).
+ */
+public class GraphNode<T> {
+    public T value;
+    public List<GraphNode<T>> neighbors;
+    public boolean visited;
+
+    public GraphNode() {
+        this.visited = false;
+        this.neighbors = new ArrayList<>();
+    }
+}

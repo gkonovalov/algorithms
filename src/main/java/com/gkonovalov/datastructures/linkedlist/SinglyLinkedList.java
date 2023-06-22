506130ba0657de07ab6283fa15617d94380e7a0a
George Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-22 11:46:35 -0700

    Added Singly Linked List data structure

--- src/main/java/com/gkonovalov/datastructures/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/gkonovalov/datastructures/linkedlist/SinglyLinkedList.java
@@ -6,10 +6,21 @@ import java.util.Objects;
  * Created by Georgiy Konovalov on 17/05/2023.
  * <p>
  * Singly Linked List implementation.
+ * A linked list is a linear data structure in which elements are stored as individual nodes,
+ * where each node contains a value and a reference (or link) to the next node in the sequence.
+ * Unlike arrays, linked lists do not require contiguous memory allocation, allowing for efficient
+ * insertion and deletion operations. In a singly linked list, each node has a reference only to the
+ * next node in the list. The last node in the list points to null, indicating the end of the list.
+ * Traversing a singly linked list involves starting from the head (the first node) and following
+ * the next references until reaching the end.
+ * Linked lists are dynamic data structures, meaning they can grow or shrink in size during program
+ * execution. They are commonly used when the number of elements is unknown or can change frequently,
+ * or when efficient insertion and deletion operations are required. However, linked lists have slower
+ * access times compared to arrays since accessing an element requires traversing the list from
+ * the beginning.
  * <p>
- * Runtime Complexity: O(1) for the {@code add}, {@code size}, {@code prepend},
- *                     O(n) for the {@code get}, {@code indexOfs}, {@code contains},
- *                                  {@code remove} and {@code append}.
+ * Runtime Complexity: O(1) for the {@code add}, {@code size}, {@code prepend}, {@code append},
+ *                     O(n) for the {@code get}, {@code indexOfs}, {@code contains}, {@code remove}.
  * Space Complexity: O(n).
  */
 public class SinglyLinkedList<T> {

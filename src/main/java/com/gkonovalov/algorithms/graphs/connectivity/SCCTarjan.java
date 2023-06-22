c48147d276e4afdbfec2289dfb7725f3bdb6f586
George Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-22 12:48:31 -0700

    Added Strongly Connected Components Tarjan algorithm

---
+++ src/main/java/com/gkonovalov/algorithms/graphs/connectivity/SCCTarjan.java
@@ -0,0 +1,101 @@
+package com.gkonovalov.algorithms.graphs.connectivity;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by Georgiy Konovalov on 21/06/2023.
+ * <p>
+ * Strongly Connected Components Tarjan algorithm implementation.
+ * The Strongly Connected Components (SCC) algorithm is used to identify groups of vertices in a directed
+ * graph where each vertex in a group is reachable from every other vertex in that group. In other words,
+ * it helps find clusters of closely interconnected vertices in a directed graph.
+ * Tarjan's algorithm is generally considered simpler to implement and suitable for various types of graphs,
+ * while Kosaraju's algorithm offers a guaranteed linear time complexity and can be parallelized effectively.
+ * </p>
+ * Runtime Complexity: O(V+E).
+ * Space Complexity: O(V).
+ */
+public class SCCTarjan {
+
+    private Stack<Integer> stack;
+
+    private int[] id;
+    private int[] low;
+
+    private int pre;
+    private int count;
+
+    private int numVertices;
+
+    public SCCTarjan(List<List<Integer>> adjList) {
+        this.numVertices = adjList.size();
+        this.stack = new Stack<>();
+        this.id = new int[numVertices];
+        this.low = new int[numVertices];
+
+        boolean[] visited = new boolean[numVertices];
+
+        for (int v = 0; v < numVertices; v++) {
+            if (!visited[v]) {
+                dfs(adjList, visited, v);
+            }
+        }
+    }
+
+    private void dfs(List<List<Integer>> adjList, boolean[] visited, int v) {
+        visited[v] = true;
+        low[v] = pre++;
+        int min = low[v];
+        stack.push(v);
+
+        for (int w : adjList.get(v)) {
+            if (!visited[w]) {
+                dfs(adjList, visited, w);
+            }
+
+            if (low[w] < min) {
+                min = low[w];
+            }
+        }
+
+        if (min < low[v]) {
+            low[v] = min;
+            return;
+        }
+
+        while (true) {
+            int w = stack.pop();
+            id[w] = count;
+            low[w] = numVertices;
+
+            if (w == v) {
+                break;
+            }
+        }
+        count++;
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public boolean isStronglyConnected(int v, int w) {
+        return id[v] == id[w];
+    }
+
+    public List<List<Integer>> stronglyConnectedComponents() {
+        List<List<Integer>> result = new LinkedList<>();
+
+        for (int i = 0; i < count; i++) {
+            result.add(new LinkedList<>());
+        }
+
+        for (int i = 0; i < id.length; i++) {
+            result.get(id[i]).add(i);
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/gkonovalov/algorithms/graphs/connectivity/SCCTarjanTest.java
@@ -0,0 +1,47 @@
+package com.gkonovalov.algorithms.graphs.connectivity;
+
+
+import com.gkonovalov.datastructures.graphs.representation.AdjacencyList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by Georgiy Konovalov on 21/06/2023.
+ * <p>
+ * Tests for {@link SCCTarjan}.
+ * </p
+ */
+public class SCCTarjanTest {
+    private SCCTarjan scc;
+
+    @BeforeEach
+    public void setUp() {
+        AdjacencyList adjList = new AdjacencyList();
+        this.scc = new SCCTarjan(adjList.getAdjacencyListDirected(true));
+    }
+
+    @Test
+    @DisplayName("Testing SCCTarjan.stronglyConnectedComponents")
+    public void testStronglyConnectedComponents() {
+        assertEquals(List.of(
+                List.of(2),
+                List.of(0, 1, 3),
+                List.of(5),
+                List.of(4)
+        ), scc.stronglyConnectedComponents());
+    }
+
+    @Test
+    @DisplayName("Testing SCCTarjan.isStronglyConnected")
+    public void testIsStronglyConnected() {
+        assertTrue(scc.isStronglyConnected(0, 1));
+        assertFalse(scc.isStronglyConnected(0, 4));
+    }
+}

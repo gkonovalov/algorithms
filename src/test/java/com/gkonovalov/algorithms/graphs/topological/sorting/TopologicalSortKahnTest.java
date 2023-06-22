2ebbe52525edd29c7bd30f4c18f4cfd97e6725b2
George Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-22 15:51:32 -0700

    Added Topological Sort Kahn algorithm

--- src/test/java/com/gkonovalov/algorithms/graphs/topological/sorting/TopologicalSortKahnTest.java
+++ src/test/java/com/gkonovalov/algorithms/graphs/topological/sorting/TopologicalSortKahnTest.java
@@ -1,6 +1,5 @@
 package com.gkonovalov.algorithms.graphs.topological.sorting;
 
-import com.gkonovalov.algorithms.graphs.topological.sorting.TopologicalSortKahn;
 import com.gkonovalov.datastructures.graphs.representation.AdjacencyList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,11 +27,11 @@ public class TopologicalSortKahnTest {
     @Test
     @DisplayName("Testing TopologicalSortKahn.topologicalSort")
     public void testTopologicalSort() {
-        int[] actualOrder = topologicalSort.topologicalSort(adjList.getAdjacencyListDirected(false));
+        int[] actualOrder = topologicalSort.topologicalSort(adjList.getGraphDirected(false));
         int[] expectedOrder = new int[]{0, 4, 1, 2, 5, 3};
 
         assertArrayEquals(expectedOrder, actualOrder);
-        assertNull(topologicalSort.topologicalSort(adjList.getAdjacencyListDirected(true)));
+        assertNull(topologicalSort.topologicalSort(adjList.getGraphDirected(true)));
     }
 
 }

b793008c36cd36fbb88cab0a2400775bda12103b
George Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-06-22 15:52:47 -0700

    Added Strongly Connected Components Tarjan algorithm

--- src/test/java/com/gkonovalov/algorithms/graphs/connectivity/scc/SCCTarjanTest.java
+++ src/test/java/com/gkonovalov/algorithms/graphs/connectivity/scc/SCCTarjanTest.java
@@ -1,7 +1,6 @@
 package com.gkonovalov.algorithms.graphs.connectivity.scc;
 
 
-import com.gkonovalov.algorithms.graphs.connectivity.scc.SCCTarjan;
 import com.gkonovalov.datastructures.graphs.representation.AdjacencyList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -25,7 +24,7 @@ public class SCCTarjanTest {
     @BeforeEach
     public void setUp() {
         AdjacencyList adjList = new AdjacencyList();
-        this.scc = new SCCTarjan(adjList.getAdjacencyListDirected(true));
+        this.scc = new SCCTarjan(adjList.getGraphDirected(true));
     }
 
     @Test

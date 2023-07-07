d58d43dda51a4ce05d6953cc943fddea830b976f
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-07-07 14:54:44 -0700

    Added MST Prim's lazy algorithm.

---
+++ src/main/java/com/gkonovalov/algorithms/graphs/mst/MSTPrimLazy.java
@@ -0,0 +1,75 @@
+package com.gkonovalov.algorithms.graphs.mst;
+
+import com.gkonovalov.datastructures.graphs.EdgeWeighted;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Georgiy Konovalov on 5/07/2023.
+ * <p>
+ * Minimum Spanning Tree - Prim's algorithm lazy implementation.
+ * The Prim's algorithm is a greedy algorithm used to find the Minimum Spanning Tree (MST) in connected
+ * undirected weighted graph (weights must be positive). The MST is a subset of the graph's edges that
+ * connects all the vertices with the minimum total edge weight and doesn't have any cycles.
+ * It starts with an arbitrary vertex and repeatedly adds the minimum-weight edge that connects a vertex
+ * in the MST to a vertex outside the MST. It grows the MST from a single vertex until it spans all vertices.
+ * In the lazy implementation of Prim's algorithm, a priority queue is used to store all the edges that connect
+ * the explored and unexplored vertices. This implementation defers the actual edge relaxation until it is needed,
+ * hence the name "lazy." It avoids unnecessary edge relaxation, improving efficiency when the graph
+ * has a large number of edges.
+ * </p>
+ * Runtime Complexity: O(E log V).
+ * Space Complexity:   O(V).
+ */
+public class MSTPrimLazy {
+
+    private PriorityQueue<EdgeWeighted> minHeap;
+    private List<EdgeWeighted> mst;
+    private boolean[] visited;
+    private double weight;
+
+    public MSTPrimLazy(List<List<EdgeWeighted>> adjListWithWeight) {
+        int numV = adjListWithWeight.size();
+        this.minHeap = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+        this.visited = new boolean[numV];
+        this.mst = new ArrayList<>();
+        this.weight = 0;
+
+        primMST(adjListWithWeight, 0);
+    }
+
+    private void primMST(List<List<EdgeWeighted>> adjListWithWeight, int startV) {
+        scan(adjListWithWeight, startV);
+
+        while (!minHeap.isEmpty()) {
+            EdgeWeighted e = minHeap.poll();
+
+            if (!visited[e.toV]) {
+                mst.add(e);
+                weight += e.weight;
+                scan(adjListWithWeight, e.toV);
+            }
+        }
+    }
+
+    private void scan(List<List<EdgeWeighted>> graph, int v) {
+        visited[v] = true;
+        for (EdgeWeighted e : graph.get(v)) {
+            if (!visited[e.toV]) {
+                minHeap.add(e);
+            }
+        }
+    }
+
+    public List<EdgeWeighted> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        return weight;
+    }
+}
+
+
---
+++ src/test/java/com/gkonovalov/algorithms/graphs/mst/MSTPrimLazyTest.java
@@ -0,0 +1,48 @@
+package com.gkonovalov.algorithms.graphs.mst;
+
+import com.gkonovalov.datastructures.graphs.EdgeWeighted;
+import com.gkonovalov.datastructures.graphs.representation.AdjacencyList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+/**
+ * Created by Georgiy Konovalov on 21/06/2023.
+ * <p>
+ * Tests for {@link MSTPrimLazy}.
+ * </p
+ */
+public class MSTPrimLazyTest {
+    private MSTPrimLazy mst;
+
+    @BeforeEach
+    public void setUp() {
+        AdjacencyList adjacencyList = new AdjacencyList();
+        this.mst = new MSTPrimLazy(adjacencyList.getGraphWeightedUndirected());
+    }
+
+    @Test
+    @DisplayName("Testing MSTPrimLazy.weight")
+    public void testWeight() {
+        assertEquals(181, mst.weight());
+    }
+
+    @Test
+    @DisplayName("Testing MSTPrimLazy.edges")
+    public void testEdges() {
+        assertEquals(mst.edges(), List.of(
+                new EdgeWeighted(0, 7, 16),
+                new EdgeWeighted(7, 1, 19),
+                new EdgeWeighted(0, 2, 26),
+                new EdgeWeighted(2, 3, 17),
+                new EdgeWeighted(7, 5, 28),
+                new EdgeWeighted(5, 4, 35),
+                new EdgeWeighted(2, 6, 40))
+        );
+    }
+}

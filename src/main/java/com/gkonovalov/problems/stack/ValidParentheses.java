12c5e026902356b1853f084c7c729a23f6e545dd
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-07-24 13:39:49 -0700

    Added Leetcode problem: [Easy][20] - Valid Parentheses

---
+++ src/main/java/com/gkonovalov/problems/stack/ValidParentheses.java
@@ -0,0 +1,46 @@
+package com.gkonovalov.problems.stack;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * Created by Georgiy Konovalov on 24/07/2023.
+ * <p>
+ *
+ * @see <a href="https://leetcode.com/problems/valid-parentheses/">
+ *          [Easy][20] - Valid Parentheses
+ *      </a>
+ * </p>
+ * Runtime Complexity: O(n) for {@code isValid},
+ * Space Complexity:   O(n) for {@code isValid}.
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println("Is valid:" + isValid("()[]{}"));
+    }
+
+    public static boolean isValid(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+
+        HashMap<Character, Character> map = new HashMap<>() {{
+            put(')', '(');
+            put('}', '{');
+            put(']', '[');
+        }};
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char letter : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == map.get(letter)) {
+                stack.pop();
+            } else {
+                stack.add(letter);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}

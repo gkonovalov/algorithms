f7b2052c1cad66788b408bfd49eb2ad2c5674db9
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-07-28 21:54:34 -0700

    Added Median of Medians algorithm implementation

--- src/main/java/com/gkonovalov/algorithms/arrays/selection/MedianOfMedians.java
+++ src/main/java/com/gkonovalov/algorithms/arrays/selection/MedianOfMedians.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 /**
  * Created by Georgiy Konovalov on 28/07/2023.
  * <p>
- * The BFPRT (Blum-Floyd-Pratt-Rivest-Tarjan) algorithm implementation, also known as the "median of medians".
- * The "median of medians" is a selection algorithm used to find the k-th smallest/biggest element in an unsorted array.
- * It improves the worst-case performance of the QuickSelect algorithm by selecting good pivot elements to
- * partition the array. The main idea of the "median of medians" algorithm is to find an approximate median of the
- * array by dividing it into groups of five elements each and then finding the median of each group. The medians of
- * these groups are used to find the "median of medians", which becomes the pivot for partitioning the array.
- * By selecting a good pivot, the "median of medians" algorithm ensures that the partition step divides the array
- * into roughly equal-sized partitions, which helps in reducing the number of comparisons required to find the
- * k-th smallest element.
- * The "median of medians" algorithm has a time complexity of O(n), where n is the number of elements in the array,
- * making it a linear-time algorithm. This makes it more efficient than the simple QuickSelect algorithm, which can
- * have a worst-case time complexity of O(n^2) in some cases.
+ * The BFPRT (Blum-Floyd-Pratt-Rivest-Tarjan) algorithm implementation, also known as the
+ * "median of medians".The "median of medians" is a selection algorithm used to find the
+ * k-th smallest/biggest element in an unsorted array. It improves the worst-case performance
+ * of the QuickSelect algorithm by selecting good pivot elements to partition the array.
+ * The main idea of the "median of medians" algorithm is to find an approximate median of the array
+ * by dividing it into groups of five elements each and then finding the median of each group.
+ * The medians of these groups are used to find the "median of medians", which becomes the pivot
+ * for partitioning the array. By selecting a good pivot, the "median of medians" algorithm ensures
+ * that the partition step divides the array into roughly equal-sized partitions, which helps in
+ * reducing the number of comparisons required to find the k-th smallest element.
+ * The "median of medians" algorithm has a time complexity of O(n), where n is the number of
+ * elements in the array, making it a linear-time algorithm. This makes it more efficient than
+ * the simple QuickSelect algorithm, which can have a worst-case time complexity of O(n^2) in some cases.
  * </p>
  * Runtime Complexity: O(n).
  * Space Complexity:   O(log n).

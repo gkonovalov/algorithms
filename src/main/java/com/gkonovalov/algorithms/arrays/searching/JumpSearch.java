22b41569c6b24830463960427bf71e28207055a0
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-07-30 14:11:33 -0700

    Added Jump Search algorithm implementation

--- src/main/java/com/gkonovalov/algorithms/arrays/searching/JumpSearch.java
+++ src/main/java/com/gkonovalov/algorithms/arrays/searching/JumpSearch.java
@@ -3,10 +3,16 @@ package com.gkonovalov.algorithms.arrays.searching;
 /**
  * Created by Georgiy Konovalov on 27/07/2023.
  * <p>
- * Jump search implementation. Jump search is a searching algorithm used on sorted arrays or lists.
- * It is a relatively simple and efficient searching technique, especially when dealing with
- * large datasets. The algorithm makes use of the idea that the data is sorted, and it performs a
- * series of jumps to quickly narrow down the search range where the target element might be present.
+ * Jump Search implementation. Jump search is a searching algorithm designed to find a specific target
+ * value in a sorted array. It is an improvement over linear search, which checks each element in the
+ * array one by one. Jump search performs fewer comparisons by "jumping" ahead in the array in
+ * fixed-sized steps. The algorithm makes use of the idea that the data is sorted, and it
+ * performs a series of jumps to quickly narrow down the search range where the target element might
+ * be present.
+ * Jump search is most effective for large, sorted arrays, especially when binary search is not applicable
+ * due to the lack of random access (e.g., when searching in linked lists) or the high cost of accessing
+ * elements (e.g., when searching in external storage). However, for small arrays, binary search or
+ * interpolation search may perform better.
  * </p>
  * Runtime Complexity: O(sqrt(n)) for the {@code search}.
  * Space Complexity:   O(1) for {@code search}.

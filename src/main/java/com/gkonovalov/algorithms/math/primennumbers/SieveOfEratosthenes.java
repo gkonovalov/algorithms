39978c7d22337e38c34e73352fb5deac140a4db6
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-08-24 16:30:10 -0700

    The Sieve of Eratosthenes algorithm implementation

---
+++ src/main/java/com/gkonovalov/algorithms/math/primennumbers/SieveOfEratosthenes.java
@@ -0,0 +1,44 @@
+package com.gkonovalov.algorithms.math.primennumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Georgiy Konovalov on 24/08/2023.
+ * <p>
+ * The Sieve of Eratosthenes algorithm implementation. The Sieve of Eratosthenes is an
+ * ancient and efficient algorithm for finding all prime numbers up to a given limit.
+ * The basic idea of the Sieve of Eratosthenes is to iteratively mark the multiples of
+ * each prime number starting from 2 (the smallest prime). As the algorithm progresses,
+ * it eliminates the composite numbers (non-prime numbers) and leaves only the prime numbers.
+ * </p>
+ * Runtime Complexity: O(n log log n) for {@code findPrimeNumbers}.
+ * Space Complexity:   O(n) for {@code findPrimeNumbers}.
+ */
+public class SieveOfEratosthenes {
+
+    public static List<Integer> findPrimeNumbers(int n) {
+        if (n <= 1) {
+            return List.of(0);
+        }
+
+        boolean[] notPrime = new boolean[n + 1];
+
+        for (int i = 2; i * i <= n; i++) {
+            if (!notPrime[i]) {
+                for (int k = i * i; k <= n; k += i) {
+                    notPrime[k] = true;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/gkonovalov/algorithms/math/SieveOfEratosthenesTest.java
@@ -0,0 +1,32 @@
+package com.gkonovalov.algorithms.math;
+
+import com.gkonovalov.algorithms.math.primennumbers.SieveOfEratosthenes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Georgiy Konovalov on 24/08/2023.
+ * <p>
+ * Tests for {@link SieveOfEratosthenes}.
+ * </p
+ */
+public class SieveOfEratosthenesTest {
+
+    private SieveOfEratosthenes sieveOfEratosthenes;
+
+    @BeforeEach
+    public void setUp() {
+        this.sieveOfEratosthenes = new SieveOfEratosthenes();
+    }
+
+    @Test
+    @DisplayName("Testing SieveOfEratosthenes.findPrimeNumbers")
+    public void testFindPrimeNumbers() {
+        assertEquals(List.of(2, 3, 5, 7), sieveOfEratosthenes.findPrimeNumbers(10));
+    }
+}

9ac7954316e1f9d5e5196469727e5184258488b2
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2023-12-15 14:50:08 -0800

    Added Leetcode problem: [Medium][161] - One Edit Distance

--- src/main/java/com/gkonovalov/problems/strings/matching/editdistance/OneEditDistance.java
+++ src/main/java/com/gkonovalov/problems/strings/matching/editdistance/OneEditDistance.java
@@ -47,4 +47,23 @@ public class OneEditDistance {
     private static int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
     }
+
+    public static boolean isOneEditDistanceIterative(String s, String t) {
+        for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
+            if (s.charAt(i) == t.charAt(i)) {
+                continue;
+            }
+
+            if (s.length() == t.length()) {
+                return s.substring(i + 1).equals(t.substring(i + 1));
+            } else if (s.length() < t.length()) {
+                return s.substring(i).equals(t.substring(i + 1));
+            } else {
+                return s.substring(i + 1).equals(t.substring(i));
+            }
+        }
+
+        return Math.abs(s.length() - t.length()) == 1;
+    }
+
 }

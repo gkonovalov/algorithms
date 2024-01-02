48ed9dd53338fb2f0c5cef3ed94224673e20bb7c
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-02 14:50:56 -0800

    The Matrix Multiplication algorithm implementation.

---
+++ src/main/java/com/gkonovalov/algorithms/math/matrix/MatrixMultiplication.java
@@ -0,0 +1,44 @@
+package com.gkonovalov.algorithms.math.matrix;
+
+/**
+ * Created by Georgiy Konovalov on 1/2/2024.
+ * <p>
+ * The Matrix Multiplication algorithm implementation.
+ * Let's consider two matrices A and B, where A is an (m x n) matrix and B is an (n x p) matrix.
+ * The resulting matrix Result, obtained by multiplying A and B, will be of size (m x p).
+ * Each element Result[i][j] is calculated by taking the dot product of the i-th row of matrix A and
+ * the j-th column of matrix B.
+ *
+ * For matrix multiplication, the number of columns in the first matrix must be equal to the number
+ * of rows in the second matrix. The resulting matrix, known as the matrix product, has the number of
+ * rows of the first and the number of columns of the second matrix. The product of matrices A and B
+ * is denoted as AB.
+ * </p>
+ * Runtime Complexity: O(m * n * p) - where A[m x n], B[n x p], Result[m x p].
+ * Space Complexity:   O(m * p).
+ */
+public class MatrixMultiplication {
+
+    public int[][] multiply(int[][] a, int[][] b) {
+        int rowsA = a.length;
+        int colsA = a[0].length;
+        int rowsB = b.length;
+        int colsB = b[0].length;
+
+        if (colsA != rowsB) {
+            throw new IllegalArgumentException("Invalid matrix dimensions for multiplication!");
+        }
+
+        int[][] result = new int[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/gkonovalov/algorithms/math/matrix/MatrixMultiplicationTest.java
@@ -0,0 +1,46 @@
+package com.gkonovalov.algorithms.math.matrix;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+/**
+ * Created by Georgiy Konovalov on 1/2/2024.
+ * <p>
+ * Tests for {@link MatrixMultiplication}.
+ * </p
+ */
+public class MatrixMultiplicationTest {
+
+    private MatrixMultiplication matrixMultiplication;
+
+    @BeforeEach
+    public void setUp() {
+        this.matrixMultiplication = new MatrixMultiplication();
+    }
+
+    @Test
+    @DisplayName("Testing MatrixMultiplication.multiply")
+    public void testMultiply() {
+        int[][] a = {
+                {2, 4, 3},
+                {6, 5, 2}
+        };
+
+        int[][] b = {
+                {0, 1, 8},
+                {2, 4, 9},
+                {1, 6, 8}
+        };
+
+        int[][] expected = {
+                {11, 36, 76},
+                {12, 38, 109}
+        };
+
+        assertArrayEquals(expected, matrixMultiplication.multiply(a, b));
+    }
+}

85e3aa537fa07271e9508ddec3e32cc8dd47f880
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-04 17:56:49 -0800

    Added Matrix Multiplication algorithm implementation

--- src/test/java/com/gkonovalov/algorithms/math/matrix/multiplication/MatrixMultiplicationTest.java
+++ src/test/java/com/gkonovalov/algorithms/math/matrix/multiplication/MatrixMultiplicationTest.java
@@ -1,6 +1,5 @@
 package com.gkonovalov.algorithms.math.matrix.multiplication;
 
-import com.gkonovalov.algorithms.math.matrix.multiplication.MatrixMultiplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;

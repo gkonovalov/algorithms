34d1d0c9e00fd37deae9292eb62f0acdc72f5a08
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:52:07 -0800

    Added Ternary Search algorithm implementation

--- src/test/java/com/gkonovalov/algorithms/arrays/searching/TernarySearchTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/searching/TernarySearchTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by Georgiy Konovalov on 27/07/2023.
+ * Created by Georgiy Konovalov on 7/27/2023.
  * <p>
  * Tests for {@link TernarySearch}.
  * </p
@@ -15,18 +15,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TernarySearchTest {
 
     private TernarySearch ternarySearch;
-    private int[] sortedArray;
 
     @BeforeEach
     public void setUp() {
         this.ternarySearch = new TernarySearch();
-        this.sortedArray = new int[]{-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
     }
 
     @Test
     @DisplayName("Testing TernarySearch.search")
     public void testSearch() {
-        assertEquals(ternarySearch.search(sortedArray, 13), 6);
-        assertEquals(ternarySearch.search(sortedArray, 9999), -1);
+        int[] arr = {-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
+
+        assertEquals(ternarySearch.search(arr, 13), 6);
+        assertEquals(ternarySearch.search(arr, 9999), -1);
     }
 }

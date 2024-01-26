76efba9e431187cfaf03b4f25b6d681dfe633999
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:03:30 -0800

    Added Quick Sort 3 way partition algorithm

--- src/test/java/com/gkonovalov/algorithms/arrays/sorting/quicksort/QuickSort3WayPartitioningTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/sorting/quicksort/QuickSort3WayPartitioningTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
- * Created by Georgiy Konovalov on 12/06/2023.
+ * Created by Georgiy Konovalov on 6/12/2023.
  * <p>
  * Tests for {@link QuickSort3WayPartitioning}.
  * </p
@@ -15,21 +15,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class QuickSort3WayPartitioningTest {
 
     private QuickSort3WayPartitioning quickSort;
-    private int[] arr;
 
     @BeforeEach
     public void setUp() {
         this.quickSort = new QuickSort3WayPartitioning();
-        this.arr = new int[]{40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
     }
 
     @Test
     @DisplayName("Testing QuickSort3WayPartitioning.sort")
     public void testSort() {
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+        int[] expected = {-555, -2, -1, 3, 4, 5, 5, 13, 20, 25, 40, 42, 44, 53};
+
         quickSort.sort(arr);
 
-        for (int i = 1; i < arr.length; i++) {
-            assertTrue(arr[i - 1] <= arr[i]);
-        }
+        assertArrayEquals(expected, arr);
     }
 }

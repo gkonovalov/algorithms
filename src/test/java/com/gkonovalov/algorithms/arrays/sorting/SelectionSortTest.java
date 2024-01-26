841bf30a00b376c353a298e85addc2da051a9c40
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:18:59 -0800

    Added Selection Sort algorithm

--- src/test/java/com/gkonovalov/algorithms/arrays/sorting/SelectionSortTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/sorting/SelectionSortTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * Created by Georgiy Konovalov on 12/05/2023.
@@ -15,21 +15,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SelectionSortTest {
 
     private SelectionSort selectionSort;
-    private int[] arr;
 
     @BeforeEach
     public void setUp() {
         this.selectionSort = new SelectionSort();
-        this.arr = new int[]{40, -2, 4, 44, 5, 42, 13, 20, -555, 25, 3, -1, 53};
     }
 
     @Test
     @DisplayName("Testing Selection Sort")
     public void testSort() {
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+        int[] expected = {-555, -2, -1, 3, 4, 5, 5, 13, 20, 25, 40, 42, 44, 53};
+
         selectionSort.sort(arr);
 
-        for (int i = 1; i < arr.length; i++) {
-            assertTrue(arr[i - 1] < arr[i]);
-        }
+        assertArrayEquals(expected, arr);
     }
 }

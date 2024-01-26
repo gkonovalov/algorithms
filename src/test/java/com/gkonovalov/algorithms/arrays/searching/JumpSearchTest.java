eaec0790c2cee55de4086fb0a1a1d09f92a8c1f6
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:47:41 -0800

    Added Jump Search algorithm implementation

--- src/test/java/com/gkonovalov/algorithms/arrays/searching/JumpSearchTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/searching/JumpSearchTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by Georgiy Konovalov on 27/07/2023.
+ * Created by Georgiy Konovalov on 7/27/2023.
  * <p>
  * Tests for {@link JumpSearch}.
  * </p
@@ -15,17 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class JumpSearchTest {
 
     private JumpSearch jumpSearch;
-    private int[] sortedArray;
 
     @BeforeEach
     public void setUp() {
         this.jumpSearch = new JumpSearch();
-        this.sortedArray = new int[]{-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
     }
 
     @Test
     @DisplayName("Testing JumpSearch.search")
     public void testSearchIteratively() {
+        int[] sortedArray = {-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
+
         assertEquals(jumpSearch.search(sortedArray, 13), 6);
         assertEquals(jumpSearch.search(sortedArray, 9999), -1);
     }

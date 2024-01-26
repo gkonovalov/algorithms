11bbfab77269ea9b9cb918e04ba9a1507b0314d5
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:53:56 -0800

    Added Median of Medians algorithm implementation

--- src/test/java/com/gkonovalov/algorithms/arrays/selection/MedianOfMediansTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/selection/MedianOfMediansTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by Georgiy Konovalov on 28/07/2023.
+ * Created by Georgiy Konovalov on 7/28/2023.
  * <p>
  * Tests for {@link MedianOfMedians}.
  * </p
@@ -15,17 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MedianOfMediansTest {
 
     private MedianOfMedians medianOfMedians;
-    private int[] arr;
 
     @BeforeEach
     public void setUp() {
         this.medianOfMedians = new MedianOfMedians();
-        this.arr = new int[]{40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
     }
 
     @Test
     @DisplayName("Testing MedianOfMedians.findKElement")
     public void testFindKElement() {
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+
         assertEquals(-555, medianOfMedians.findKElement(arr, 1, false));
         assertEquals(53, medianOfMedians.findKElement(arr, 1, true));
         assertEquals(44, medianOfMedians.findKElement(arr, 2, true));

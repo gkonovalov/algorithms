c5aa6ae5e51f117334c70efb3ef5865ad50b9852
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:18:18 -0800

    Added Merge Sort algorithm

--- src/test/java/com/gkonovalov/algorithms/arrays/sorting/MergeSortTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/sorting/MergeSortTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * Created by Georgiy Konovalov on 18/05/2023.
@@ -15,31 +15,31 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MergeSortTest {
 
     private MergeSort mergeSort;
-    private int[] arr;
 
     @BeforeEach
     public void setUp() {
         this.mergeSort = new MergeSort();
-        this.arr = new int[]{40, -2, 4, 44, 5, 42, 5, 13, 20, -555, 25, 3, -1, 53};
     }
 
     @Test
     @DisplayName("Testing MergeSort.sort")
     public void testSort() {
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+        int[] expected = {-555, -2, -1, 3, 4, 5, 5, 13, 20, 25, 40, 42, 44, 53};
+
         mergeSort.sort(arr);
 
-        for (int i = 1; i < arr.length; i++) {
-            assertTrue(arr[i - 1] <= arr[i]);
-        }
+        assertArrayEquals(expected, arr);
     }
 
     @Test
     @DisplayName("Testing MergeSort.sort2")
     public void testSort2() {
-        mergeSort.sort2(arr);
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+        int[] expected = {-555, -2, -1, 3, 4, 5, 5, 13, 20, 25, 40, 42, 44, 53};
 
-        for (int i = 1; i < arr.length; i++) {
-            assertTrue(arr[i - 1] <= arr[i]);
-        }
+        mergeSort.sort(arr);
+
+        assertArrayEquals(expected, arr);
     }
 }

ce0b6a4067241e7bdf47433c028acbd5385107d4
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:46:48 -0800

    Added Exponential Search implementation

--- src/test/java/com/gkonovalov/algorithms/arrays/searching/ExponentialSearchTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/searching/ExponentialSearchTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by Georgiy Konovalov on 30/07/2023.
+ * Created by Georgiy Konovalov on 7/30/2023.
  * <p>
  * Tests for {@link ExponentialSearch}.
  * </p
@@ -15,17 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExponentialSearchTest {
 
     private ExponentialSearch exponentialSearch;
-    private int[] sortedArray;
 
     @BeforeEach
     public void setUp() {
         this.exponentialSearch = new ExponentialSearch();
-        this.sortedArray = new int[]{-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
     }
 
     @Test
     @DisplayName("Testing ExponentialSearch.search")
     public void testSearch() {
+        int[] sortedArray = {-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
+
         assertEquals(exponentialSearch.search(sortedArray, 13), 6);
         assertEquals(exponentialSearch.search(sortedArray, 9999), -1);
     }

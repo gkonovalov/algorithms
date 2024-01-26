d17f5797a2c2c3bfd3dabe719fc0fb27da3498eb
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:47:14 -0800

    Added Interpolation Search algorithm implementation

--- src/test/java/com/gkonovalov/algorithms/arrays/searching/InterpolationSearchTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/searching/InterpolationSearchTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Created by Georgiy Konovalov on 30/07/2023.
+ * Created by Georgiy Konovalov on 7/30/2023.
  * <p>
  * Tests for {@link InterpolationSearch}.
  * </p
@@ -15,17 +15,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class InterpolationSearchTest {
 
     private InterpolationSearch interpolationSearch;
-    private int[] sortedArray;
 
     @BeforeEach
     public void setUp() {
         this.interpolationSearch = new InterpolationSearch();
-        this.sortedArray = new int[]{-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
     }
 
     @Test
     @DisplayName("Testing InterpolationSearch.search")
     public void testSearchIteratively() {
+        int[] sortedArray = {-555, -2, -1, 3, 4, 5, 13, 20, 25, 40, 42, 44, 53};
+
         assertEquals(interpolationSearch.search(sortedArray, 13), 6);
         assertEquals(interpolationSearch.search(sortedArray, 9999), -1);
     }

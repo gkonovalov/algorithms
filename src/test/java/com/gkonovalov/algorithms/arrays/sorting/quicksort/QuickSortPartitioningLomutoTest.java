b2d87b57fdda8c5a65182549c6041d5e5d0982ec
Georgiy Konovalov <22642112+gkonovalov@users.noreply.github.com>
2024-01-26 15:04:55 -0800

    Added Quick Sort Partitioning Lomuto algorithm

--- src/test/java/com/gkonovalov/algorithms/arrays/sorting/quicksort/QuickSortPartitioningLomutoTest.java
+++ src/test/java/com/gkonovalov/algorithms/arrays/sorting/quicksort/QuickSortPartitioningLomutoTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
- * Created by Georgiy Konovalov on 18/05/2023.
+ * Created by Georgiy Konovalov on 5/18/2023.
  * <p>
  * Tests for {@link QuickSortPartitionLomuto}.
  * </p
@@ -15,21 +15,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class QuickSortPartitioningLomutoTest {
 
     private QuickSortPartitionLomuto quickSort;
-    private int[] arr;
 
     @BeforeEach
     public void setUp() {
         this.quickSort = new QuickSortPartitionLomuto();
-        this.arr = new int[]{40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
     }
 
     @Test
     @DisplayName("Testing QuickSortPartitionLomuto.sort")
     public void testRandomizedSortHoare() {
+        int[] arr = {40, -2, 4, 44, 5, 5, 42, 13, 20, -555, 25, 3, -1, 53};
+        int[] expected = {-555, -2, -1, 3, 4, 5, 5, 13, 20, 25, 40, 42, 44, 53};
+
         quickSort.sort(arr);
 
-        for (int i = 1; i < arr.length; i++) {
-            assertTrue(arr[i - 1] <= arr[i]);
-        }
+        assertArrayEquals(expected, arr);
     }
 }
